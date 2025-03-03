233d655dccac8faa9b9446172825842dcfed78da
Ashirasan <fluk290344@gmail.com>
2025-03-03 21:43:36 +0700

    auth locker record

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,12 @@
 import { AuthRouteModule } from "./routes/auth.route";
+import { LockerRouteModule } from "./routes/locker.route";
+import { RecordRouteModule } from "./routes/record.route";
 // import { HomeRouteModule } from "./routes/home.route";
 
 const routes = [
   { prefix: "/auth", routes: AuthRouteModule },
+  { prefix: "/locker", routes: LockerRouteModule },
+  { prefix: "/record", routes: RecordRouteModule }
   //   { prefix: "/home", routes: HomeRouteModule },
 ];
 
