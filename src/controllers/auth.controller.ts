99bdd5b928545592743a96b95b41fcc60356e80e
Ashirasan <fluk290344@gmail.com>
2025-03-04 14:20:53 +0700

    add lastname in register and login

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -8,7 +8,8 @@ export class AuthController extends ControllerModule {
     // await this.prisma.<database_table>.<method>
     let email: string = req.body.email;
     let password: string = req.body.password;
-    let name: string = req.body.name;
+    let firstname: string = req.body.firstname;
+    let lastname: string = req.body.lastname;
     let bcryptPassword: string = await bcrypt.hash(
       password,
       Number(process.env.BCRYPT_ROUNDS)
@@ -17,9 +18,24 @@ export class AuthController extends ControllerModule {
       const check: string = await this.prisma
         .$queryRaw`SELECT email FROM users WHERE email = ${email}`;
       if (check.length === 0) {
-        const result: Object = await this.prisma
-          .$queryRaw`INSERT INTO users (users.email,users.password,users.name) VALUES (${email}, ${bcryptPassword}, ${name})`;
-        res.status(200).json({ message: "register complete" });
+        const result: any[] = await this.prisma
+          .$queryRaw`INSERT INTO users (users.email,users.password,users.firstname,users.lastname) VALUES (${email}, ${bcryptPassword}, ${firstname},${lastname})`;
+          console.log();
+          const id : any[] = await this.prisma.$queryRaw`SELECT LAST_INSERT_ID() AS user_id`;
+          console.log();
+          const user_id : number = Number(id[0].user_id);
+          const token = jwt.sign(
+            { id: user_id},
+            String(process.env.SECRET_KEY)
+          );
+          res.status(200).json({
+            message: "Register complete",
+            user_id: user_id,
+            email: email,
+            firstname: firstname,
+            lastname: lastname,
+            token: token,
+          });
       } else {
         res.status(400).json({ message: "this email is already used" });
       }
@@ -53,7 +69,8 @@ export class AuthController extends ControllerModule {
             message: "login complete",
             user_id: checkemail[0].user_id,
             email: checkemail[0].email,
-            name: checkemail[0].name,
+            firstname: checkemail[0].firstname,
+            lastname: checkemail[0].lastname,
             token: token,
           });
         } else {
