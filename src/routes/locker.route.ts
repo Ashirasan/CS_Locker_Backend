25b28f3b466dbc8ec2879b6caaa8ba2e65b425a6
Ashirasan <fluk290344@gmail.com>
2025-03-06 00:40:09 +0700

    use token in user id

--- src/routes/locker.route.ts
+++ src/routes/locker.route.ts
@@ -3,24 +3,24 @@ import { LockerController } from "../controllers/locker.controller";
 
 export class LockerRouteModule extends RouteModule {
     private controller: LockerController = new LockerController();
-
+    
     loadRoutes(): void {
         this.router.post("/reservation-locker",this.middleware.verifyAuth(),  (req, res) => 
             this.controller.reservationLocker(req,res)
         );
-        this.router.delete("/cancel-locker/:rsv_id",this.middleware.verifyAuth(),  (req,res)=>
+        this.router.post("/cancel-locker",this.middleware.verifyAuth(),  (req,res)=>
             this.controller.cancelLocker(req,res)
         );
         this.router.get("/get-locker", (req,res)=>
             this.controller.getLocker(req,res)
         );
-        this.router.get("/get-reservelocker/:user_id",this.middleware.verifyAuth(), (req,res)=>
+        this.router.get("/get-reservelocker",this.middleware.verifyAuth(), (req,res)=>
             this.controller.getReserveLocker(req,res)
         );
 
         //unlock
         //by app
-        this.router.get("/unlock-locker-app/:rsv_id",this.middleware.verifyAuth(),  (req,res)=>
+        this.router.post("/unlock-locker-app",this.middleware.verifyAuth(),  (req,res)=>
             this.controller.unlockLockerApp(req,res)
         );
         //by board
@@ -28,7 +28,7 @@ export class LockerRouteModule extends RouteModule {
             this.controller.unlockLockerBoard(req,res)
         );
 
-        this.router.get("/record-get/:user_id", this.middleware.verifyAuth(), (req,res)=>
+        this.router.get("/record-get", this.middleware.verifyAuth(), (req,res)=>
             this.controller.getRecord(req,res)
         );
     }
